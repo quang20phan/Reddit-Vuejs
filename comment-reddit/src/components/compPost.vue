52872df4150d5f136f0760057d6fe567846831f3
dongtenquang <tacatv2002@gmail.com>
2023-11-17 00:45:24 +0700

    update

--- comment-reddit/src/components/compPost.vue
+++ comment-reddit/src/components/compPost.vue
@@ -19,67 +19,71 @@
           </a>
         </div>
       </div>
-      <div class="post">
-        <div class="post_description">
-          <span class="post_description_container">
-            <a href="">
-              <img
-                :src="author.author_avatar"
-                alt=""
-              />
-              {{ author.username }}
-            </a>
-            <span style="color: #798389">• {{ post.date }}</span>
-          </span>
-        </div>
-        <div class="post_join">
-          <button class="btn">Join</button>
-          <i class="fa-solid fa-ellipsis icon-ellipsis"></i>
-        </div>
-      </div>
-      <div class="post-title" style="padding: 0 16px; font-weight: bold">
-        <span style="font-size: 18px">{{ post.title }}</span>
-      </div>
-    </div>
-    <div class="post-body">
-      <p style="color: ##1a1a1b; font-size: 14px">
-        {{ post.body }}
-      </p>
-    </div>
-    <div class="post-img">
-      <img :src="post.image_url" alt="">
-    </div>
-    <div class="footer-post">
-      <div class="right">
-        <div class="box">
-          <i
-            style="margin: 0 6px 0 10px; font-size: 16px"
-            class="fal fa-arrow-alt-up"
-          ></i>
-          <span>{{post.upvote}}</span>
-          <i
-            style="margin: 0 10px 0 6px; font-size: 16px"
-            class="fal fa-arrow-alt-down"
-          ></i>
-        </div>
-        <div class="box">
-          <i
-            style="margin: 0 6px 0 10px; font-size: 16px"
-            class="fal fa-comment-alt-minus"
-          ></i>
-          <span style="margin-right: 10px">{{ post.comment_count }}</span>
-        </div>
-      </div>
-      <div class="left">
-        <div class="box">
-          <i
-            style="margin: 0 6px 0 10px; font-size: 16px"
-            class="fal fa-share"
-          ></i>
 
-          <span style="margin-right: 10px">Share</span>
+    </div>
+    <div class="post-item">
+      <div class="post" style="display: flex; justify-content: space-between;">
+          <div class="post_description">
+            <span class="post_description_container">
+              <a href="">
+                <img
+                  :src="author.author_avatar"
+                  alt=""
+                />
+                {{ author.username }}
+              </a>
+              <span style="color: #798389">• {{ post.date }}</span>
+            </span>
+          </div>
+          <div class="post_join" style="display: flex; align-items: center;">
+            <button class="btn">Join</button>
+            <i class="fa-solid fa-ellipsis icon-ellipsis"></i>
+          </div>
         </div>
-      </div>
+        <div class="post-title" style="height: auto; width: 100%; padding: 0 16px; font-weight: bold">
+          <span style="font-size: 18px">{{ post.title }}</span>
+        </div>
+        <div class="post-body">
+          <p style="color: ##1a1a1b; font-size: 14px">
+            {{ post.body }}
+          </p>
+        </div>
+        <div class="post-img">
+          <img :src="post.image_url" alt="">
+        </div>
+        <div class="footer-post">
+          <div class="right">
+            <div class="box">
+              <i
+                style="margin: 0 6px 0 10px; font-size: 16px"
+                class="fal fa-arrow-alt-up"
+              ></i>
+              <span>{{post.upvote}}</span>
+              <i
+                style="margin: 0 10px 0 6px; font-size: 16px"
+                class="fal fa-arrow-alt-down"
+              ></i>
+            </div>
+            <div class="box">
+              <i
+                style="margin: 0 6px 0 10px; font-size: 16px"
+                class="fal fa-comment-alt-minus"
+              ></i>
+              <span style="margin-right: 10px">{{ post.comment_count }}</span>
+            </div>
+          </div>
+          <div class="left">
+            <div class="box">
+              <i
+                style="margin: 0 6px 0 10px; font-size: 16px"
+                class="fal fa-share"
+              ></i>
+
+              <span style="margin-right: 10px">Share</span>
+            </div>
+          </div>
+
+        </div>
     </div>
   </div>
 </template>
